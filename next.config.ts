7444d073e34b6833958c7a6c4c8b763d10bae153
Szabo Zoltan <szabo.zolika.88@gmail.com>
2025-02-19 11:32:14 +0200

    feat: added styled components

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  compiler: {
+    styledComponents: {
+      ssr: true,
+      minify: true,
+    },
+  },
 };
 
 export default nextConfig;
