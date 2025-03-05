84f54e4456119cb1c447ff34463d569ca7dc16ea
Szabo Zoltan <szabo.zolika.88@gmail.com>
2025-03-05 12:18:11 +0200

    feat: keeps now playing screen on

--- src/components/complex/gamePlaying/GamePlaying.tsx
+++ src/components/complex/gamePlaying/GamePlaying.tsx
@@ -42,6 +42,34 @@ const GamePlaying: React.FC<IProps> = ({ gameIdentifier }) => {
     };
   }, [gameIdentifier]);
 
+  useEffect(() => {
+    let wakeLock: WakeLockSentinel | null = null;
+    if ('wakeLock' in navigator) {
+      const requestWakeLock = async () => {
+        try {
+          wakeLock = await navigator.wakeLock.request('screen');
+        } catch (err) {
+          const { name, message } = err as Error;
+          console.error(`Could not obtain wake lock: ${name}, ${message}`);
+        }
+      };
+
+      document.addEventListener('visibilitychange', () => {
+        if (wakeLock !== null && document.hidden) {
+          wakeLock.release().then(() => console.info('Wake lock on page visibility change.'));
+        } else if (!document.hidden) {
+          requestWakeLock();
+        }
+      });
+
+      requestWakeLock();
+    }
+
+    return () => {
+      wakeLock?.release();
+    };
+  }, []);
+
   const handleAmend: ComponentProps<typeof PointButtons>['liveAmend'] = (player, point) => {
     if (game) {
       const newGame = { ...game };
