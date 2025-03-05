0a4a9d69bc70aa5bf67dc6905bae651977abd32a
Szabo Zoltan <szabo.zolika.88@gmail.com>
2025-03-05 14:09:49 +0200

    feat: live update on success

--- src/components/complex/pointButtons/PointButtons.tsx
+++ src/components/complex/pointButtons/PointButtons.tsx
@@ -5,21 +5,24 @@ import { Container, DecreaseButton, IncreaseButton } from './styled';
 import { createClient } from '@/utils/supabase/client';
 import { Minus, Plus } from 'lucide-react';
 
+type IPlayerKey = keyof Pick<Game, 'player1_point' | 'player2_point'>;
+
 interface IProps {
   gameIdentifier: string;
-  player: keyof Pick<Game, 'player1_point' | 'player2_point'>;
-  liveAmend: (player: keyof Pick<Game, 'player1_point' | 'player2_point'>, point: number) => void;
+  player: IPlayerKey;
+  liveAmend: (player: IPlayerKey, point: number) => void;
 }
 
 const supabase = createClient();
 
 const PointButtons: React.FC<IProps> = ({ gameIdentifier, player, liveAmend }) => {
   const handleAddPoint = (point: number) => {
-    liveAmend(player, point);
-    supabase.rpc(player === 'player1_point' ? 'add_point_1' : 'add_point_2', {
-      game_identifier: gameIdentifier,
-      point: point,
-    });
+    supabase
+      .rpc(player === 'player1_point' ? 'add_point_1' : 'add_point_2', {
+        game_identifier: gameIdentifier,
+        point: point,
+      })
+      .then(() => liveAmend(player, point));
   };
 
   return (
