0ffad9bd13d0799b9ee742591ec24a28e919386e
Szabo Zoltan <szabo.zolika.88@gmail.com>
2025-03-20 18:51:33 +0200

    feat: modal for listen words

--- src/components/complex/listenButton/ListenButton.tsx
+++ src/components/complex/listenButton/ListenButton.tsx
@@ -4,49 +4,92 @@ import { Spinner } from '@/components/basic/spinner/Spinner';
 import { createClient } from '@/utils/supabase/client';
 import { Mic, MicOff } from 'lucide-react';
 import { useEffect, useState } from 'react';
-import { MicButton } from './styled';
+import { BUttons, Dialog, MicButton } from './styled';
 import useListen from '@/hooks/useListen';
+import Button from '@/components/basic/button/Button';
+import Input from '@/components/basic/input/Input';
 
 interface IProps {
   gameIdentifier: string;
   liveUpdate: (player: 'player1_point' | 'player2_point', point: number) => void;
 }
 
-const LISTEN_LIST = ['first', 'second'];
-
 const supabase = createClient();
 
 const ListenButton: React.FC<IProps> = ({ gameIdentifier, liveUpdate }) => {
+  const [listenValues, setListenValues] = useState(['first', 'second']);
   const [listening, setListening] = useState(false);
   const [updating, setUpdating] = useState(false);
-  const [listenControls, hearing] = useListen(LISTEN_LIST);
+  const [listenControls, hearing] = useListen(listenValues);
+  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     console.log('trigger for:', hearing);
-    if (hearing && LISTEN_LIST.includes(hearing)) {
+    if (hearing && listenValues.includes(hearing)) {
       setUpdating(true);
       supabase
-        .rpc(hearing === LISTEN_LIST[0] ? 'add_point_1' : 'add_point_2', {
+        .rpc(hearing === listenValues[0] ? 'add_point_1' : 'add_point_2', {
           game_identifier: gameIdentifier,
           point: 1,
         })
         .then(() => {
-          liveUpdate(hearing === LISTEN_LIST[0] ? 'player1_point' : 'player2_point', 1);
+          liveUpdate(hearing === listenValues[0] ? 'player1_point' : 'player2_point', 1);
           setUpdating(false);
         });
     }
-  }, [hearing, gameIdentifier, liveUpdate]);
+  }, [hearing, gameIdentifier, liveUpdate, listenValues]);
 
   const handleMicChange = () => {
-    try {
-      listenControls[listening ? 'stop' : 'start']();
-    } catch (error) {
-      console.warn(error);
+    if (listening) {
+      try {
+        listenControls.stop();
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setListening(false);
+      }
+    } else {
+      setModalOpen(true);
     }
-    setListening(!listening);
   };
 
-  return <MicButton onClick={handleMicChange}>{updating ? <Spinner /> : listening ? <Mic /> : <MicOff />}</MicButton>;
+  const handleStartListen = () => {
+    try {
+      listenControls.start();
+      setModalOpen(false);
+    } catch (error) {
+      console.warn(error);
+    } finally {
+      setListening(true);
+    }
+  };
+
+  return (
+    <>
+      <MicButton onClick={handleMicChange}>{updating ? <Spinner /> : listening ? <Mic /> : <MicOff />}</MicButton>
+      <Dialog open={modalOpen}>
+        <h1>Keywords to listen for players</h1>
+        <label>
+          <span>Player 1: </span>
+          <Input
+            value={listenValues[0]}
+            onChange={(event) => setListenValues((prev) => [event.target.value, prev[1]])}
+          />
+        </label>
+        <label>
+          <span>Player 2: </span>
+          <Input
+            value={listenValues[1]}
+            onChange={(event) => setListenValues((prev) => [prev[0], event.target.value])}
+          />
+        </label>
+        <BUttons>
+          <Button onClick={() => setModalOpen(false)}>Cancel</Button>
+          <Button onClick={handleStartListen}>Start listening</Button>
+        </BUttons>
+      </Dialog>
+    </>
+  );
 };
 
 export default ListenButton;
--- src/components/complex/listenButton/styled.ts
+++ src/components/complex/listenButton/styled.ts
@@ -8,3 +8,17 @@ export const MicButton = styled(Button)`
   height: 4rem;
   width: 4rem;
 `;
+
+export const Dialog = styled.dialog`
+  display: ${({ open }) => (open ? 'grid' : 'none')};
+  gap: 1rem;
+  justify-items: center;
+  padding: 2rem;
+  border-radius: var(--border-radius-3);
+`;
+
+export const BUttons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
